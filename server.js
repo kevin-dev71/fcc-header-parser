293d72f0bc473fff533f73f2dbf97c9f35ea8725
Kevin Dev <kevin.developer71@gmail.com>
2021-06-22 20:06:32 -0600

    header parser

--- server.js
+++ server.js
@@ -25,6 +25,16 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get("/api/whoami", (req, res) => {
+  let myIP = req.header("X-Forwarded-For").split(',')[0];  // X-Forwarded-For method gets three IP addresses--client, proxy1, proxy 2
+  let myLanguage = req.header('Accept-Language');
+  let mySystem = req.header('User-Agent');
+  res.json({
+    ipaddress: myIP,
+    language: myLanguage,
+    software: mySystem
+  });
+});
 
 
 // listen for requests :)
